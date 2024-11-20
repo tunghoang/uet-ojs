3508d3f6faf7d2db18927b55c12bda3d2f6838bf
hbinhng <me@hbinhng.dev>
2024-11-20 08:20:55 +0000

    Add missing config files

---
+++ vue.config.js
@@ -0,0 +1,50 @@
+var path = require("path");
+
+module.exports = {
+  css: {
+    extract: {
+      filename: "../styles/build.css",
+    },
+  },
+  chainWebpack: (config) => {
+    // Don't create an index.html file
+    config.plugins.delete("html");
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+
+    // Don't copy the /public dir
+    config.plugin("copy").tap(() => {
+      return [];
+    });
+  },
+  configureWebpack: {
+    // Don't create a map file for the js package
+    devtool: false,
+    entry: ["@/styles/_global.less", "./js/load.js"],
+    optimization: {
+      // Don't split vendor and app into separate JS files
+      splitChunks: false,
+    },
+    output: {
+      filename: "build.js",
+      // Prevent lots of files from being created when running npm run dev
+      hotUpdateChunkFilename: "hot-updates/hot-update.js",
+      hotUpdateMainFilename: "hot-updates/hot-update.json",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "lib/ui-library/src"),
+      },
+    },
+    watch: false,
+  },
+  outputDir: path.resolve(__dirname, "js"),
+  runtimeCompiler: true,
+  // Part of the vue2-dropzone library is not transpiled
+  // as part of the normal build process, which results
+  // in errors in < IE 11. This directive makes sure the
+  // dependencies are included when babel transpiles code
+  // See: https://github.com/rowanwins/vue-dropzone/issues/439
+  // See: https://stackoverflow.com/a/58949645/1723499
+  transpileDependencies: ["vue2-dropzone"],
+};
